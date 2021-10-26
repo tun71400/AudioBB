c68a95b5e5f02e7ba9b5c169ebbd493ef2ee290e
tun71400 <77815590+tun71400@users.noreply.github.com>
2021-10-26 18:25:24 -0400

    Added in the Book and Booklist class

--- app/src/main/java/edu/temple/audiobb/BookList.kt
+++ app/src/main/java/edu/temple/audiobb/BookList.kt
@@ -1,6 +1,26 @@
 package edu.temple.audiobb
 
-class BookList {
+import java.io.Serializable;
 
-    
+class BookList: Serializable{
+
+    var datalist = mutableListOf<Book>()
+
+    fun add(book: Book){
+        datalist.add(book)
+    }
+
+    fun remove(book: Book){
+        datalist.remove(book)
+    }
+
+    fun get(index: Int):Book {
+        return datalist[index]
+    }
+
+    fun size():Int{
+        return datalist.size
+    }
+
+
 }
