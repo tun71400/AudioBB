cf11c4f0ed6009a82f0dafa3aab2532a6f9e63e8
tun71400 <77815590+tun71400@users.noreply.github.com>
2021-10-27 14:51:45 -0400

    Revert "Added in 10 books"
    This reverts commit 7cd28464

--- app/src/main/java/edu/temple/audiobb/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobb/MainActivity.kt
@@ -7,17 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var newbook:BookList = BookList()
-            newbook.add(Book("Space is Big","Tom H."))
-            newbook.add(Book("Cube Houses","Sarah L."))
-            newbook.add(Book("Lighting from the Sky","Ryan S."))
-            newbook.add(Book("Grumpy the Mouse","Lauren Y."))
-            newbook.add(Book("The Art of Pillows","Derick W."))
-            newbook.add(Book("The Yawn at Dawn","Tim U."))
-            newbook.add(Book("Phone Pieces and Parts Volume 2","Sam N."))
-            newbook.add(Book("The perfect Bowtie","Lee Y."))
-            newbook.add(Book("Friends and Enemies","Will W."))
-            newbook.add(Book("The Last Call","Norm A."))
     }
 }
